65821fe77ea200a62311e61c84387855791deba1
deepak-devtron <devtron@devtrons-MacBook-Pro.local>
2022-06-30 21:31:35 +0530

    fixing failure of getAttributesByKey API

--- AttributesRouter/GetAttributesByKey.go
+++ AttributesRouter/GetAttributesByKey.go
@@ -6,11 +6,12 @@ import (
 )
 
 func (suite *AttributeRouterTestSuite) TestClassA2GetAttributesByKey() {
-	fileData := Base.ReadAnyJsonFile("../testUtils/credentials.json")
+	envConfig := Base.ReadBaseEnvConfig()
+	baseCredentials := Base.ReadAnyJsonFile(envConfig.BaseCredentialsFile)
 	suite.Run("A=1=AttributesWithValidValueOfKey", func() {
 		queryParams := map[string]string{"key": "url"}
 		attributesApiResp := HitGetAttributesApi(queryParams, suite.authToken)
-		assert.Equal(suite.T(), fileData.BaseServerUrl, attributesApiResp.Result.Value)
+		assert.Equal(suite.T(), baseCredentials.BaseServerUrl, attributesApiResp.Result.Value)
 	})
 
 	suite.Run("A=2=AttributesWithInvalidValueOfKey", func() {
