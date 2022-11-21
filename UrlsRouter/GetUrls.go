5eb6ec2ade72f99cb4beb1bb8e4fac0522feca57
Gireesh Naidu <gireesh@devtron.ai>
2022-11-21 20:59:29 +0530

    test for api token user

--- UrlsRouter/GetUrls.go
+++ UrlsRouter/GetUrls.go
@@ -9,60 +9,100 @@ import (
 )
 
 func (suite *UrlsTestSuite) TestGetUrlsForHelmApp() {
+	testGetUrlsForHelmApp(suite, suite.authToken)
+}
+func testGetUrlsForHelmApp(suite *UrlsTestSuite, token string) {
 	envConf, _ := helmRouter.GetEnvironmentConfigForHelmApp()
 	queryParams := map[string]string{"appId": envConf.HAppId}
 	log.Println("Hitting Get urls API")
-	resp := HitGetUrls(queryParams, suite.authToken, GetUrlsUrlHelm)
+	if token == "" {
+		token = suite.authToken
+	}
+	resp := HitGetUrls(queryParams, token, GetUrlsUrlHelm)
 	assert.Equal(suite.T(), 200, resp.Code)
 }
 func (suite *UrlsTestSuite) TestGetUrlsForHelmAppWithIncorrectAppId() {
+	testGetUrlsForHelmAppWithIncorrectAppId(suite, suite.authToken)
+}
+func testGetUrlsForHelmAppWithIncorrectAppId(suite *UrlsTestSuite, token string) {
 	randomHAppId := testUtils.GetRandomNumberOf9Digit()
 	queryParams := map[string]string{"appId": strconv.Itoa(randomHAppId)}
-	resp := HitGetUrls(queryParams, suite.authToken, GetUrlsUrlHelm)
+	if token == "" {
+		token = suite.authToken
+	}
+	resp := HitGetUrls(queryParams, token, GetUrlsUrlHelm)
 	assert.Equal(suite.T(), 400, resp.Code)
 	assert.Equal(suite.T(), "Bad Request", resp.Status)
 }
-
 func (suite *UrlsTestSuite) TestGetUrlsForDevtronApp() {
+	testGetUrlsForDevtronApp(suite, suite.authToken)
+}
+func testGetUrlsForDevtronApp(suite *UrlsTestSuite, token string) {
 	envConf, _ := GetEnvironmentConfigForDevtronApp()
 	queryParams := map[string]string{"appId": envConf.AppId, "envId": envConf.EnvId}
 	log.Println("Hitting Get urls API")
-	resp := HitGetUrls(queryParams, suite.authToken, GetUrlsUrl)
+	if token == "" {
+		token = suite.authToken
+	}
+	resp := HitGetUrls(queryParams, token, GetUrlsUrl)
 	assert.Equal(suite.T(), 200, resp.Code)
 }
-
 func (suite *UrlsTestSuite) TestGetUrlsForDevtronAppWithIncorrectAppId() {
+	testGetUrlsForDevtronAppWithIncorrectAppId(suite, suite.authToken)
+}
+func testGetUrlsForDevtronAppWithIncorrectAppId(suite *UrlsTestSuite, token string) {
 	randomInstalledAppId := "installedAppId-1"
 	randomEnvId := "envid-1"
 	queryParams := map[string]string{"installedAppId": randomInstalledAppId, "envId": randomEnvId}
-	resp := HitGetUrls(queryParams, suite.authToken, GetUrlsUrl)
+	if token == "" {
+		token = suite.authToken
+	}
+	resp := HitGetUrls(queryParams, token, GetUrlsUrl)
 	assert.Equal(suite.T(), 400, resp.Code)
 	assert.Equal(suite.T(), "Bad Request", resp.Status)
 }
 
 func (suite *UrlsTestSuite) TestGetUrlsForInstalledApp() {
+	testGetUrlsForInstalledApp(suite, suite.authToken)
+}
+func testGetUrlsForInstalledApp(suite *UrlsTestSuite, token string) {
 	envConf, _ := GetEnvironmentConfigForInstalledApp()
 	queryParams := map[string]string{"installedAppId": envConf.InstalledAppId, "envId": envConf.EnvId}
 	log.Println("Hitting Get urls API")
-	resp := HitGetUrls(queryParams, suite.authToken, GetUrlsUrl)
+	if token == "" {
+		token = suite.authToken
+	}
+	resp := HitGetUrls(queryParams, token, GetUrlsUrl)
 	assert.Equal(suite.T(), 200, resp.Code)
 }
 
 func (suite *UrlsTestSuite) TestGetUrlsForInstalledAppWithIncorrectAppId() {
+	testGetUrlsForInstalledAppWithIncorrectAppId(suite, suite.authToken)
+}
+func testGetUrlsForInstalledAppWithIncorrectAppId(suite *UrlsTestSuite, token string) {
 	randomAppId := "appId-1"
 	randomEnvId := "envid-1"
 	queryParams := map[string]string{"appId": randomAppId, "envId": randomEnvId}
-	resp := HitGetUrls(queryParams, suite.authToken, GetUrlsUrl)
+	if token == "" {
+		token = suite.authToken
+	}
+	resp := HitGetUrls(queryParams, token, GetUrlsUrl)
 	assert.Equal(suite.T(), 400, resp.Code)
 	assert.Equal(suite.T(), "Bad Request", resp.Status)
 }
 
 func (suite *UrlsTestSuite) TestGetUrlsdata() {
+	testGetUrlsdata(suite, suite.authToken)
+}
+func testGetUrlsdata(suite *UrlsTestSuite, token string) {
 	expected := GetTestExpectedUrlsData()
 	envConf, _ := GetEnvironmentConfigForDevtronApp()
 	queryParams := map[string]string{"appId": envConf.AppId, "envId": envConf.EnvId}
 	log.Println("Hitting Get urls API")
-	resp := HitGetUrls(queryParams, suite.authToken, GetUrlsUrl)
+	if token == "" {
+		token = suite.authToken
+	}
+	resp := HitGetUrls(queryParams, token, GetUrlsUrl)
 	assert.Equal(suite.T(), 200, resp.Code)
 	assert.Equal(suite.T(), 3, len(resp.Result))
 	for j, _ := range resp.Result {
@@ -74,3 +114,43 @@ func (suite *UrlsTestSuite) TestGetUrlsdata() {
 		}
 	}
 }
+
+//func (suite *UrlsTestSuite) TestUrlsdata() {
+//	//create api-token
+//	createApiTokenRequestDTO := ApiTokenRouter.GetPayLoadForCreateApiToken()
+//	payloadForCreateApiTokenRequest, _ := json.Marshal(createApiTokenRequestDTO)
+//	responseOfCreateApiToken := ApiTokenRouter.HitCreateApiTokenApi(string(payloadForCreateApiTokenRequest), suite.authToken)
+//	token := responseOfCreateApiToken.Result.Token
+//	//update user with permissions
+//	updateUserDto, roleGroupId := userRouter.CreateUserRequestPayload("GroupsAndRoleFilter", suite.authToken)
+//	updateUserDto.Id = int32(responseOfCreateApiToken.Result.UserId)
+//	updateUserDto.EmailId = "API-TOKEN:" + createApiTokenRequestDTO.Name
+//	byteValueOfStruct, _ = json.Marshal(updateUserDto)
+//
+//	//test with token user
+//	suite.TestGetUrlsdata(token)
+//	suite.TestGetUrlsForInstalledAppWithIncorrectAppId(token)
+//	suite.TestGetUrlsForInstalledApp(token)
+//	suite.TestGetUrlsForDevtronAppWithIncorrectAppId(token)
+//	suite.TestGetUrlsForDevtronApp(token)
+//	suite.TestGetUrlsForHelmAppWithIncorrectAppId(token)
+//	suite.TestGetUrlsForHelmApp(token)
+//
+//	//delete user before deleting token
+//
+//	//delete created token
+//	var tokenId int
+//	responseOfGetAllApiTokens := ApiTokenRouter.HitGetAllApiTokens(suite.authToken).Result
+//	for _, result := range responseOfGetAllApiTokens {
+//		if result.UserId == responseOfCreateApiToken.Result.UserId {
+//			assert.Equal(suite.T(), responseOfCreateApiToken.Result.Token, result.Token)
+//			assert.Equal(suite.T(), createApiTokenRequestDTO.ExpireAtInMs, result.ExpireAtInMs)
+//			assert.Equal(suite.T(), createApiTokenRequestDTO.Name, result.Name)
+//			tokenId = result.Id
+//		}
+//	}
+//	log.Println("=== Here We Deleting the Token After Verification")
+//	responseOfDeleteApi := ApiTokenRouter.HitDeleteApiToken(strconv.Itoa(tokenId), suite.authToken)
+//	assert.True(suite.T(), responseOfDeleteApi.Result.Success)
+//
+//}
